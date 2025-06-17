e7972b7c3281c11c997cea790cf882f8757838a5
yhli <liyahui360@163.com>
2025-06-17 13:46:57 +0800

    远程打印设置

--- src/views/system/syssettingconfig/component/printConfig.vue
+++ src/views/system/syssettingconfig/component/printConfig.vue
@@ -16,24 +16,26 @@
 				<el-row :gutter="10" style="padding: 0px 40px 0px">
 					<el-form :model="serverform" label-width="auto">
 						<el-form-item label="授权码">
-							<el-input style="width: 340px" v-model="printConfig.app.machineId" placeholder="请输入授权码" />
+							<el-input style="width: 340px" :disabled="isEnabled" v-model="printConfig.app.machineId"
+								placeholder="请输入授权码" />
 							<el-button type="primary" style="margin-left: 10px" @click="btnCon">测试连接</el-button>
 						</el-form-item>
 						<el-form-item :label="item.printType === 0 ? '病例打印' : '小票打印'"
 							v-for="item in printConfig.appSettings">
 							<el-select :placeholder="item.printType === 0 ? '请选择病例打印' : '请选择小票打印'" style="width: 240px"
-								v-model="item.printName">
+								:disabled="isEnabled" v-model="item.printName">
 								<el-option v-for="item in printDevies" :key="item.name" :label="item.name"
 									:value="item.name" />
 							</el-select>
 							<el-input v-model="item.printPage" type="number" min="1" v-if="item.printType === 0"
-								style="max-width: 150px;margin-left: 10px;">
+								:disabled="isEnabled" style="max-width: 150px;margin-left: 10px;">
 								<template #append>份数</template>
 							</el-input>
 						</el-form-item>
 						<el-form-item>
-							<el-button type="info" @click="cancel">{{ '取消修改' }}</el-button>
-							<el-button type="primary" @click="save">保 存</el-button>
+							<el-button type="info" v-if="isEnabled" @click="edit">{{ '编辑' }}</el-button>
+							<el-button type="info" v-if="!isEnabled" @click="cancel">{{ '取消修改' }}</el-button>
+							<el-button type="primary" v-if="!isEnabled" @click="save">保 存</el-button>
 						</el-form-item>
 					</el-form>
 				</el-row>
@@ -48,6 +50,8 @@ import { ElMessageBox } from 'element-plus';
 import { hiprint } from 'vue-plugin-hiprint-signalr';
 import { getAPI } from '/@/utils/axios-utils';
 import { PrintSettingApi } from '/@/api-services/api';
+
+const isEnabled = ref(true); // 是否启用打印机服务
 
 const serverform = ref({
 	machineId: '',
@@ -75,13 +79,7 @@ const printDevies = ref<any>([]); // 打印机列表
 
 const btnCon = async () => {
 	if (printConfig.value.app.machineId) {
-		hiprint.hiwebSocket.getPrintById(printConfig.value.app.machineId);
-		hiprint.refreshPrinterList((res: any) => {
-			printDevies.value = res;
-			getAPI(PrintSettingApi).sernumberNoGetPrintConnectionIdGet(printConfig.value.app.machineId).then((res: any) => {
-				printConfig.value.app = res.data.result ?? {};
-			});
-		});
+		await getPrint(printConfig.value.app.machineId);
 	} else {
 		ElMessageBox.alert('请输入授权码', '提示', {
 			type: 'warning',
@@ -92,36 +90,44 @@ const btnCon = async () => {
 	}
 };
 
+const getPrint = async (machineId: string) => {
+	if (machineId) {
+		// 获取打印机列表
+		hiprint.hiwebSocket.getPrintById(machineId);
+		hiprint.refreshPrinterList((res: any) => {
+			printDevies.value = res;
+			getAPI(PrintSettingApi).sernumberNoGetPrintConnectionIdGet(machineId).then((res: any) => {
+				printConfig.value.app = res.data.result ?? {};
+			});
+		});
+	}
+};
+
 const init = async () => {
 	// 获取打印机配置
-	await getAPI(PrintSettingApi).getPrintSettingConfigGet().then((res: any) => {
+	await getAPI(PrintSettingApi).getPrintSettingConfigGet().then(async (res: any) => {
 		printConfig.value = res.data.result ?? {};
+		await getPrint(printConfig.value.app.machineId);
 	});
+};
+
+const edit = () => {
+	isEnabled.value = !isEnabled.value; // 切换编辑状态 
 };
 
 /**
  * 取消修改
  */
-const cancel = () => {
-	ElMessageBox.confirm('是否取消修改？', '提示', {
-		type: 'warning',
-		showClose: false,
-		showCancelButton: true,
-		confirmButtonText: '确定',
-		cancelButtonText: '取消',
-	}).then(async() => {
-		await init();
-	}).catch(() => {
-		return;
-	});
+const cancel = async () => {
+	isEnabled.value = true; // 取消修改后，重新启用编辑
+	await init();
 };
 
 const save = async () => {
 	// 保存打印机配置
-	await getAPI(PrintSettingApi).printIdAddPrintSettingPost(printConfig.value.app.id,printConfig.value.appSettings).then((res: any) => {
-	 
-				init(); 
-		 
+	await getAPI(PrintSettingApi).printIdAddPrintSettingPost(printConfig.value.app.id, printConfig.value.appSettings).then((res: any) => {
+		init();
+		isEnabled.value = true; // 取消修改后，重新启用编辑
 	});
 };
 
